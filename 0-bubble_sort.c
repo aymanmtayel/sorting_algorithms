54660f19b94f942d2956816b1d1f7d99c208d4fa
Ayman Tayel <eng.aymantayel@gmail.com>
2024-02-17 22:33:39 +0200

    Task00 fix the loops ranges

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,14 +10,17 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, tmp = 0;
+	size_t i, j;
+	int tmp;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
@@ -25,4 +28,5 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
+	}
 }
